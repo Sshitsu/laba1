85112f8fb19fe83ff2770eb3e041956b595bedbd
Roman Parubov <global2456@mail.ru>
2025-04-11 12:33:20 +0700

    ok

---
+++ src/main/java/Classes/Dish.java
@@ -0,0 +1,22 @@
+package Classes;
+
+import java.sql.Time;
+
+
+public  class Dish extends ObjectFromMenu {
+    private int grams;
+
+    public Dish(String name, double cost, int grams, Time timeToCook) {
+        super(name, cost,timeToCook);
+        this.grams = grams;
+
+    }
+    public int getGrams() {
+        return grams;
+    }
+
+    @Override
+    public String toString() {
+        return "It's a dish the " + getName() + ", it cost=" + getCost() + ", and time to cook " + getTimeToCook().toString() + ", and " + getGrams() + " grams ";
+    }
+}
